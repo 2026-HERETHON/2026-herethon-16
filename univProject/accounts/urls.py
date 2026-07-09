315f748537cad83448ca31d90fcd8ed5a4ac3337
yunjin-grit <gracelee0730@naver.com>
2026-07-09 19:21:22 +0900

    feat: 회원가입, 로그인, 로그아웃 세션 기능 구현 완료

---
+++ univProject/accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'accounts'
+
+urlpatterns = [    
+    path('signup/', views.signUp, name='signUp'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+]
