f634cd9e5396cef39c26d328e618f29ce53874e5
ylly5 <gracelee0730@naver.com>
2026-07-11 14:35:43 +0900

    Feat: User 모델 구현 (#12)
    * feat: 회원가입, 로그인, 로그아웃 세션 기능 구현 완료
    
    * feat: User 모델  마이그레이션 적용

--- univProject/accounts/models.py
+++ univProject/accounts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+<<<<<<< HEAD
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    nickname = models.CharField(max_length=50, blank=True)
+    STATUS_CHOICES = [
+    ('survey', 'Survey'),
+    ('exploring', 'Exploring'),
+    ('selected', 'Selected'),
+    ]
+    exploration_status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='survey')
+    #최종 전공
+    #selected_major = models.ForeignKey(flow.Major, on_delete=models.SET_NULL, null=True, blank=True, related_name='selected_users')
+    #추천 전공 2개
+    #recommended_major1 = models.ForeignKey(flow.Major, on_delete=models.SET_NULL, null=True, blank=True, related_name='recommended_as_first')
+    #recommended_major2 = models.ForeignKey(flow.Major, on_delete=models.SET_NULL, null=True, blank=True, related_name='recommended_as_second')
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
+=======
+
+# Create your models here.
+>>>>>>> dev
