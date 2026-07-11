282cdd47f9df910778d70e335110f3c5cf6b4464
이석현 <jrkenniya@gmail.com>
2026-07-12 05:14:53 +0900

    feat: 전공 선택하기 (비교 선택) UI 수정

--- univProject/search/static/js/confirm.js
+++ univProject/search/static/js/confirm.js
@@ -1,8 +1,14 @@
-const startButton = document.querySelector(".startButton");
-const majorCards = document.querySelectorAll(".majorCard");
+const backIcon = document.querySelector(".material-symbols-outlined.backIcon"); // 이전 버튼
+const startButton = document.querySelector(".startButton"); // "이 전공으로 시작하기" CTA 버튼
+const majorCards = document.querySelectorAll(".majorCard"); // 전공 카드
 
 // 초기 상태 (CTA 비활성화)
 startButton.classList.add("disabled");
+
+// 이전 버튼 (이전 페이지로 이동)
+backIcon.addEventListener("click", () => {
+  window.history.back();
+});
 
 // 전공 카드 선택
 majorCards.forEach((card) => {
