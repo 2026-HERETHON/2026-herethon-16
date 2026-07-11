247d5e5b483bacfdab64dc92e8ac6123713bbe83
이석현 <jrkenniya@gmail.com>
2026-07-12 06:59:09 +0900

    feat: 추천 전공 1 UI 수정

--- univProject/search/static/js/recommend.js
+++ univProject/search/static/js/recommend.js
@@ -1,5 +1,11 @@
-// 전공 체험 시작 버튼
-const startButton = document.querySelector(".startButton");
+/* 추천 전공 - 1, 2 화면 */
+const backIcon = document.querySelector(".material-symbols-outlined.backIcon"); // 이전 버튼
+const startButton = document.querySelector(".startButton"); // 전공 체험 시작 버튼
+
+// 이전 버튼 (이전 페이지로 이동)
+backIcon.addEventListener("click", () => {
+  window.history.back();
+});
 
 // 전공 체험 시작 버튼 -> experience1.html (체험수업1 화면) 로 이동
 startButton.addEventListener("click", () => {
