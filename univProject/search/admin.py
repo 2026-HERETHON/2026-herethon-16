ba5579498ad36910d2ec7341543f0dc13fb1bfe1
yunjin-grit <gracelee0730@naver.com>
2026-07-11 17:45:40 +0900

    feat: 설문,  전공 추천, 전공 체험  모델 구현 및 admin 페이지 등록

--- univProject/search/admin.py
+++ univProject/search/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import (
+    Major,
+    Question,
+    Choice,
+    ChoiceScore,
+    ScoreRecord,
+    Experience,
+    ExperienceQuestion,
+)
+
+admin.site.register(Major)
+admin.site.register(Question)
+admin.site.register(Choice)
+admin.site.register(ChoiceScore)
+admin.site.register(ScoreRecord)
+admin.site.register(Experience)
+admin.site.register(ExperienceQuestion)
