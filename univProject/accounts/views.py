3c2aba049dc6b501f4ca291fdd9fdda9684d212f
ylly5 <gracelee0730@naver.com>
2026-07-13 19:54:19 +0900

    feat: 입학 flow 탐색 flow 연결 (#35)

--- univProject/accounts/views.py
+++ univProject/accounts/views.py
@@ -11,7 +11,7 @@ def signUp(request):
                 password=request.POST['password']
             )				
             print('회원가입 성공')
-            return redirect('onboarding')
+            return redirect('intro')
         else:
             print('회원가입 실패')
             return render(request, 'signup.html')
@@ -26,7 +26,7 @@ def login(request):
         if user is not None:
             auth_login(request, user)
             print('로그인 성공')
-            return redirect('onboarding')
+            return redirect('intro')
         else: 
             print('로그인 실패')
             errorMessage = "아이디 또는 비밀번호가 잘못되었습니다." 
@@ -39,12 +39,9 @@ def logout(request):
     print('로그아웃 성공')
     return redirect('onboarding')
 
-
-# 🌟 인트로 화면 보여주는 함수
 def intro(request):
     return render(request, 'intro.html')
 
-# 🌟 랜딩 화면 보여주는 함수
 def landing(request):
     return render(request, 'landing.html')
 
