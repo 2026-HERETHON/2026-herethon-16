165ae49fdbf7f8be2bcc3696dd71bd1bfa107b38
yunjin-grit <gracelee0730@naver.com>
2026-07-13 19:52:03 +0900

    feat: 입학 flow 탐색 flow 연결

--- univProject/accounts/models.py
+++ univProject/accounts/models.py
@@ -18,12 +18,7 @@ class UserProfile(models.Model):
     recommendedMajor1 = models.ForeignKey('search.Major', on_delete=models.SET_NULL, null=True, blank=True, related_name='recommendedAsFirst')
     recommendedMajor2 = models.ForeignKey('search.Major', on_delete=models.SET_NULL, null=True, blank=True, related_name='recommendedAsSecond')
 
-    exploration_status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='survey')
-
     def __str__(self):
-
-        return f"{self.user.username}'s profile"
-
 
         return f"{self.user.username}'s profile"
 
